1c3ce02e4837dfad56db04d1eeaaff3e9848e0c7
GuoDCZ <richard_zhong@sjtu.edu.cn>
2023-07-27 20:14:05 +0800

    initial commit

---
+++ task.py
@@ -0,0 +1,50 @@
+import openai
+import argparse
+import os
+import time
+
+def cmpl_request(prompt):
+    messages = [{'role':'user','content':prompt}]
+    cmpl = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo", 
+        messages = messages, 
+        temperature = 1.0,
+        presence_penalty = .0, 
+        frequency_penalty = .0
+    )
+    response = cmpl["choices"][0]["message"]['content']
+    return response
+
+def parse_command_line():
+    parser = argparse.ArgumentParser(description="A simple chatBot using Openai gpt-3.5-turdo model")
+    parser.add_argument('file', type=str, nargs=1,
+                        help='Specify a file as a prompt collections.')
+    return parser.parse_args()
+
+def task(fileName, repetition):
+    f = open(fileName)
+    line = f.readline()
+    q = 0
+    while line:
+        print(f'[Q{q+1}]')
+        print(line.strip('\n'))
+        for a in range(0, repetition):
+            try:
+                response = cmpl_request(line)
+                print(f'[A{a+1}]')
+                print(response)
+            except:
+                time.sleep(1)
+                a -= 1
+        line = f.readline()
+        print()
+        q+=1
+    f.close()
+
+def main():
+    args = parse_command_line()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    task(args.file[0], 20)
+
+if __name__ == '__main__':
+    main()
---
+++ task.sh
@@ -0,0 +1,2 @@
+export all_proxy="socks5://127.0.0.1:7891/"
+proxychains3 python3 task.py $*
